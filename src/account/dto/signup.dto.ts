3ddcf0dc131ee39375277e9891c74547aea8eb96
Adam Mustaoha <matawalle4u@gmail.com>
2024-12-18 11:39:20 +0100

    Added Schema exmple payload

--- src/account/dto/signup.dto.ts
+++ src/account/dto/signup.dto.ts
@@ -1,12 +1,25 @@
 import { IsEmail, IsString, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class SignupDto {
+  @ApiProperty({
+    description: 'A valid email of the user',
+    example: 'adam@byteflow.com.ng',
+  })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    description: 'Username',
+    example: 'adam4u',
+  })
   @IsString()
   username: string;
 
+  @ApiProperty({
+    description: '8 Character long password',
+    example: 'P@55word1234',
+  })
   @IsString()
   @MinLength(8, { message: 'Password must be at least 8 characters long' })
   password: string;
