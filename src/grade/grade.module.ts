045863acfefd29f22ff886e3b0fd90e23bd47a7f
Adam Mustaoha <matawalle4u@gmail.com>
2024-12-20 18:30:22 +0100

    Added GradeHistoryRepository to the TypeOrm import

--- src/grade/grade.module.ts
+++ src/grade/grade.module.ts
@@ -4,9 +4,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { GradeService } from './grade.service';
 import { GradeController } from './grade.controller';
 import { GradeCriteriaRepository } from './repositories/grade-criteria.repository';
+import { Grade } from './entities/grade.entity';
+import { GradeHistoryRepository } from './repositories/grade-history.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([GradeCriteriaRepository])],
+  imports: [
+    TypeOrmModule.forFeature([
+      GradeCriteriaRepository,
+      Grade,
+      GradeHistoryRepository,
+    ]),
+  ],
   controllers: [GradeController],
   providers: [GradeService],
 })
