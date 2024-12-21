831967bdb42b9c56f73afd57af2a921977eb7fca
Adam Mustaoha <matawalle4u@gmail.com>
2024-12-21 12:02:34 +0100

    Added sample payload on swagger

--- src/account/dto/change-password.dto.ts
+++ src/account/dto/change-password.dto.ts
@@ -1,10 +1,18 @@
 import { IsString, IsNotEmpty, MinLength } from 'class-validator';
-
+import { ApiProperty } from '@nestjs/swagger';
 export class ChangePasswordDto {
+  @ApiProperty({
+    description: 'User current password',
+    example: 'Password1234!',
+  })
   @IsString()
   @IsNotEmpty()
   currentPassword: string;
 
+  @ApiProperty({
+    description: 'New password',
+    example: '1234!Pass',
+  })
   @IsString()
   @IsNotEmpty()
   @MinLength(6)
