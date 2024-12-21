0b2283aa0e69113f10d35a7c7be81b22dbabe692
Adam Mustaoha <matawalle4u@gmail.com>
2024-12-21 12:56:26 +0100

    Updated payment reference sample data

--- src/payment/dto/verify-payment.dto.ts
+++ src/payment/dto/verify-payment.dto.ts
@@ -1,6 +1,11 @@
 import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class VerifyPaymentDto {
+  @ApiProperty({
+    description: 'Payment Reference',
+    example: 'xyz1234uvw',
+  })
   @IsNotEmpty()
   @IsString()
   reference: string;
