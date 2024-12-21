0054bb60398edd8914d6eeac676fdc13208fdcef
Adam Mustaoha <matawalle4u@gmail.com>
2024-12-21 12:37:35 +0100

    Initiate payment payload sample

--- src/payment/dto/initiate-payment.dto.ts
+++ src/payment/dto/initiate-payment.dto.ts
@@ -1,14 +1,26 @@
 import { IsEmail, IsNotEmpty, IsString, IsNumber } from 'class-validator';
-
+import { ApiProperty } from '@nestjs/swagger';
 export class InitiatePaymentDto {
+  @ApiProperty({
+    description: 'Name of the payer',
+    example: 'Adam Mustapha',
+  })
   @IsNotEmpty()
   @IsString()
   name: string;
 
+  @ApiProperty({
+    description: 'Payer email address',
+    example: 'adam@byteflow.com.ng',
+  })
   @IsNotEmpty()
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    description: 'Amount in figures',
+    example: 2000,
+  })
   @IsNotEmpty()
   @IsNumber()
   amount: number;
