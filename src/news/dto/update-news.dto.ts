0b0fac54c4675fec5cfdc39b0c2dfb722bc72da4
Adam Mustaoha <matawalle4u@gmail.com>
2024-12-22 08:58:01 +0100

    Added swagger documentation documentation

--- src/news/dto/update-news.dto.ts
+++ src/news/dto/update-news.dto.ts
@@ -1,15 +1,30 @@
 import { IsOptional, IsString } from 'class-validator';
-
+import { ApiProperty } from '@nestjs/swagger';
 export class UpdateNewsDto {
   @IsOptional()
   @IsString()
+  @ApiProperty({
+    example: 'NIM News title',
+    description: 'Title Of the news post',
+    required: true,
+  })
   title?: string;
 
   @IsOptional()
   @IsString()
+  @ApiProperty({
+    example: 'News content',
+    description: 'Content of the News',
+    required: true,
+  })
   content?: string;
 
   @IsOptional()
   @IsString()
+  @ApiProperty({
+    example: 'Adam Mustapha',
+    description: 'Name of the news author',
+    required: false,
+  })
   author?: string;
 }
