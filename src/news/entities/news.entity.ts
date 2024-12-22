0840af18ee65dc88ab95e059b7a9117a22539774
Adam Mustaoha <matawalle4u@gmail.com>
2024-12-22 08:22:44 +0100

    Added swagger documentation data samples

--- src/news/entities/news.entity.ts
+++ src/news/entities/news.entity.ts
@@ -1,17 +1,32 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
-
+import { ApiProperty } from '@nestjs/swagger';
 @Entity('news')
 export class News {
   @PrimaryGeneratedColumn()
   id: number;
 
   @Column()
+  @ApiProperty({
+    example: 'NIM News title',
+    description: 'Title Of the news post',
+    required: true,
+  })
   title: string;
 
   @Column('text')
+  @ApiProperty({
+    example: 'News content',
+    description: 'Content of the News',
+    required: true,
+  })
   content: string;
 
   @Column()
+  @ApiProperty({
+    example: 'Adam Mustapha',
+    description: 'Name of the news author',
+    required: false,
+  })
   author: string;
 
   @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
