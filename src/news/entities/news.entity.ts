f68d6c6346f4b01c0bae4f7388ccc87ebbdc500d
Adam Mustaoha <matawalle4u@gmail.com>
2024-12-22 08:16:24 +0100

    Created news entity for TypeOrm

---
+++ src/news/entities/news.entity.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('news')
+export class News {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column('text')
+  content: string;
+
+  @Column()
+  author: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
+}
