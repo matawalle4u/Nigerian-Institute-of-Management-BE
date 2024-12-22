d08d125387dc193915454023f33b1561c07e3a7c
Adam Mustaoha <matawalle4u@gmail.com>
2024-12-22 08:53:27 +0100

    Added swagger decorators for sample data

--- src/news/dto/create-news.dto.ts
+++ src/news/dto/create-news.dto.ts
@@ -1,15 +1,30 @@
 import { IsNotEmpty, IsString } from 'class-validator';
-
+import { ApiProperty } from '@nestjs/swagger';
 export class CreateNewsDto {
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({
+    example: 'NIM News title',
+    description: 'Title Of the news post',
+    required: true,
+  })
   title: string;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({
+    example: 'News content',
+    description: 'Content of the News',
+    required: true,
+  })
   content: string;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({
+    example: 'Adam Mustapha',
+    description: 'Name of the news author',
+    required: false,
+  })
   author: string;
 }
