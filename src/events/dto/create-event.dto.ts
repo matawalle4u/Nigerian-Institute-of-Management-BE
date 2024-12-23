a446c55d04901f0efa78ef033dd05cd6ceb6cd65
Adam Mustaoha <matawalle4u@gmail.com>
2024-12-23 16:53:05 +0100

    Added API Property

--- src/events/dto/create-event.dto.ts
+++ src/events/dto/create-event.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsString,
   IsNotEmpty,
@@ -5,14 +6,26 @@ import {
   IsDateString,
 } from 'class-validator';
 export class CreateEventDto {
+  @ApiProperty({
+    description: 'Name of the Event',
+    example: 'Annual Conference',
+  })
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({
+    description: 'Event Description',
+    example: 'Description goes here',
+  })
   @IsString()
   @IsOptional()
   description?: string;
 
+  @ApiProperty({
+    description: 'Event Date',
+    example: '12/12/2025',
+  })
   @IsDateString()
   date: string;
 }
