0335df9e7bd0e9d3574db34cc7063056be73b14e
Adam Mustaoha <matawalle4u@gmail.com>
2024-12-23 16:09:25 +0100

    Changed Module name

--- src/licence/licence.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { LicenceService } from './licence.service';
-import { LicenceController } from './licence.controller';
-
-@Module({
-  providers: [LicenceService],
-  controllers: [LicenceController]
-})
-export class LicenceModule {}
