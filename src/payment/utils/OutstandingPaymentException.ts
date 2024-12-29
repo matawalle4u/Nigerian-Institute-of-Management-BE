8ac1347c3bb48b2902a553aaedf63d48ebf17bc9
Adam Mustaoha <matawalle4u@gmail.com>
2024-12-29 18:43:40 +0100

    Added PaymentOutstandingExeption

---
+++ src/payment/utils/OutstandingPaymentException.ts
@@ -0,0 +1,7 @@
+import { HttpException, HttpStatus } from "@nestjs/common";
+
+export class PaymentOutStandingException extends HttpException {
+  constructor(message: string) {
+    super(message, HttpStatus.PAYMENT_REQUIRED);
+  }
+}
