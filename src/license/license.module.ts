300da4f33cdbbbace85db2088847c88285f43a9c
Adam Mustaoha <matawalle4u@gmail.com>
2024-12-29 18:47:20 +0100

    Registered JWTModule

--- src/license/license.module.ts
+++ src/license/license.module.ts
@@ -5,9 +5,17 @@ import { License } from './entities/license.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AccountModule } from 'src/account/account.module';
 import { Login } from 'src/account/entities/login.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([License, Login]), AccountModule],
+  imports: [
+    TypeOrmModule.forFeature([License, Login]),
+    JwtModule.register({
+      secret: 'Yan2Mak!!',
+      signOptions: { expiresIn: '1h' },
+    }),
+    AccountModule,
+  ],
   providers: [LicenseService],
   controllers: [LicenseController],
 })
