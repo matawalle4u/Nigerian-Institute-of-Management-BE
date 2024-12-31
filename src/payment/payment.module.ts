894fb51d9d10d786b68118b7d1670b2f879f1610
Adam Mustaoha <matawalle4u@gmail.com>
2024-12-31 19:47:19 +0100

    Registered the Licence module in the payment module

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -4,9 +4,10 @@ import { PaymentController } from './payment.controller';
 import { Payment } from './entities/payment.entity';
 import { Login } from '../account/entities/login.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { License } from 'src/license/entities/license.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Payment, Login])],
+  imports: [TypeOrmModule.forFeature([Payment, Login, License])],
   providers: [PaymentService],
   controllers: [PaymentController],
 })
