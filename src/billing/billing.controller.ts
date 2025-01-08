7397c5b98e58e32d6f9a781959a9667ba5dd7c43
Adam Mustaoha <matawalle4u@gmail.com>
2025-01-08 22:03:56 +0100

    Added billing controler

--- src/billing/billing.controller.ts
+++ src/billing/billing.controller.ts
@@ -1,4 +1,3 @@
-// src/billing/billing.controller.ts
 import { Controller, Post, Body, Param } from '@nestjs/common';
 import { BillingService } from './billing.service';
 import { CreateBillDto } from './dto/create-billing.dto';
@@ -19,12 +18,8 @@ export class BillingController {
     return this.billingService.createBillForAllUsers(description, amount);
   }
 
-  @Post('pay/:userId/:billId')
-  async processPayment(
-    @Param('userId') userId: string,
-    @Param('billId') billId: string,
-    @Body() paymentData: any,
-  ) {
-    return this.billingService.processPayment(userId, billId, paymentData);
+  @Post('pay/:billId')
+  async processPayment(@Param('billId') billId: string) {
+    return this.billingService.processPayment(billId);
   }
 }
