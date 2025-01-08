6d061a921611a97d7135c2eb3309951bad1fbf89
Adam Mustaoha <matawalle4u@gmail.com>
2025-01-08 10:57:52 +0100

    Fixed interswitch payment with test account

--- src/payment/providers/interswitch.provider.ts
+++ src/payment/providers/interswitch.provider.ts
@@ -3,25 +3,74 @@ import axios from 'axios';
 import { PaymentProvider } from '../interfaces/payment-provider.interface';
 import { InitiatePaymentDto } from '../dto/initiate-payment.dto';
 
+interface TokenResponse {
+  access_token: string;
+  expires_in: number;
+  token_type: string; // Optional: "Bearer"
+}
+
 @Injectable()
 export class InterswitchProvider implements PaymentProvider {
   private readonly interswitchBaseUrl = process.env.INTERSWITCH_URL;
   private readonly interswitchSecretKey = process.env.INTERSWITCH_SECRET_KEY;
 
+  private readonly tokenUrl = 'https://qa.interswitchng.com/api/v1/token'; // Adjust based on environment
+  private readonly clientId = '<your_client_id>';
+  private readonly clientSecret = process.env.NTERSWITCH_SECRET_KEY;
+  private readonly baseUrl = 'https://qa.interswitchng.com/api/v3/purchases';
+
+//   curl -X 'POST' \
+//   https://qa.interswitchng.com/api/v3/purchases \
+//   -H 'Authorization: Bearer eyJhbGciOiJSUzI1NiJ9.eyJhdWQiOlsiaXN3LWNvbGxlY3Rpb25zIiwiaXN3LXBheW1lbnRnYXRld2F5IiwicGFzc3BvcnQiLCJwcm9qZWN0LXgtbWVyY2hhbnQiLCJ2YXVsdCJdLCJtZXJjaGFudF9jb2RlIjoiTVg2MDcyIiwicmVxdWVzdG9yX2lkIjoiMTIzODA4NTk1MDMiLCJzY29wZSI6WyJwcm9maWxlIl0sImp0aSI6IjVkOTczM2Y5LWMzNDEtNGFjZC04ZjE3LWViYzUyYWE0NjM2MiIsInBheWFibGVfaWQiOiIzMzU5NyIsImNsaWVudF9pZCI6IklLSUFCMjNBNEUyNzU2NjA1QzFBQkMzM0NFM0MyODdFMjcyNjdGNjYwRDYxIn0.ElgBX2KoF9LuUUpeBGzzp8CDAllTHWfgM6pJRgTtPYGJpoZufKlJrmE4QTvZV6MIVaNtK21majTgR4qXJr7CEkPK_4zCIHyN2b8a445vqhLYcbffQvK4EeUn_RzsWTmub2bruG5s4bRS1il5itPR0QQ-trEsbELU7TAHvC4p786RiAQd-K_I0bwtLzIXQN65jlw3eJxxK-BGfca-OMTUo9HGvraebfLB-7h4-vNbPred58gfLBSwK31jaLP19cMRc5Jea28jrlmGNUhHGzjnP7ZanqgC9uuvoepQsa39_DNBonR6xirxKw4aNlNLcKOTn026wyOTHIHUGlDQ3s3AOQ' \
+//   -H 'Content-Type: application/json' \
+//   -H 'accept: application/json' \
+//   -d '{
+//  "customerId": "customer@test.com",
+//  "amount": "10000",
+//  "transactionRef": "456erTYds",
+//  "currency": "NGN",
+//  "authData": "G3cf/VTtAHCdHZNxc5GXWRI8z5P0goL2amXWDVFgb6D3XK/QMtZW90TYdl5zffDCNpiZThJzk0+eEU/Y/aYS6fyIOpQZGFrOr8hmvx5869sl2kr5u8qjnM7q5b4ZnTqdKDLtNxr3Qr7anj6YLpox1FOsiyT26mktXL+7SFOaZ15NMtne1z4xrj4R2SndowI/Znsapo7Gfzvp+L7XJyQ8kLYYRk3INjvmRPPQoJg1R0Nnh6EQE3ldIdwylB7GKtr6a71N/yCd4ZtyIcqq1ZNzdWcZyy5eEBAlDIxuECdBqH6hRq2/RbkfARqidNN4Kq0WviSRaRYGbiNjl2W9pNcM8g=="
+// }'
+
   async initializePayment(
     initiatePaymentDto: InitiatePaymentDto,
   ): Promise<any> {
+    //const token = await this.getAccessToken();
+    const authData =
+      'G3cf/VTtAHCdHZNxc5GXWRI8z5P0goL2amXWDVFgb6D3XK/QMtZW90TYdl5zffDCNpiZThJzk0+eEU/Y/aYS6fyIOpQZGFrOr8hmvx5869sl2kr5u8qjnM7q5b4ZnTqdKDLtNxr3Qr7anj6YLpox1FOsiyT26mktXL+7SFOaZ15NMtne1z4xrj4R2SndowI/Znsapo7Gfzvp+L7XJyQ8kLYYRk3INjvmRPPQoJg1R0Nnh6EQE3ldIdwylB7GKtr6a71N/yCd4ZtyIcqq1ZNzdWcZyy5eEBAlDIxuECdBqH6hRq2/RbkfARqidNN4Kq0WviSRaRYGbiNjl2W9pNcM8g==';
+    const token =
+      'eyJhbGciOiJSUzI1NiJ9.eyJhdWQiOlsiaXN3LWNvbGxlY3Rpb25zIiwiaXN3LXBheW1lbnRnYXRld2F5IiwicGFzc3BvcnQiLCJwcm9qZWN0LXgtbWVyY2hhbnQiLCJ2YXVsdCJdLCJtZXJjaGFudF9jb2RlIjoiTVg2MDcyIiwicmVxdWVzdG9yX2lkIjoiMTIzODA4NTk1MDMiLCJzY29wZSI6WyJwcm9maWxlIl0sImp0aSI6IjVkOTczM2Y5LWMzNDEtNGFjZC04ZjE3LWViYzUyYWE0NjM2MiIsInBheWFibGVfaWQiOiIzMzU5NyIsImNsaWVudF9pZCI6IklLSUFCMjNBNEUyNzU2NjA1QzFBQkMzM0NFM0MyODdFMjcyNjdGNjYwRDYxIn0.ElgBX2KoF9LuUUpeBGzzp8CDAllTHWfgM6pJRgTtPYGJpoZufKlJrmE4QTvZV6MIVaNtK21majTgR4qXJr7CEkPK_4zCIHyN2b8a445vqhLYcbffQvK4EeUn_RzsWTmub2bruG5s4bRS1il5itPR0QQ-trEsbELU7TAHvC4p786RiAQd-K_I0bwtLzIXQN65jlw3eJxxK-BGfca-OMTUo9HGvraebfLB-7h4-vNbPred58gfLBSwK31jaLP19cMRc5Jea28jrlmGNUhHGzjnP7ZanqgC9uuvoepQsa39_DNBonR6xirxKw4aNlNLcKOTn026wyOTHIHUGlDQ3s3AOQ';
+    const purchasePayload = { ...initiatePaymentDto, authData: authData };
+    console.log(purchasePayload);
     const response = await axios.post(
-      `${this.interswitchBaseUrl}`,
+      `${this.baseUrl}`,
       //   { amount, email, description },
-      initiatePaymentDto,
+      purchasePayload,
       {
-        headers: { Authorization: `Bearer ${this.interswitchSecretKey}` },
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
       },
     );
     return response.data;
   }
 
+  async getAccessToken(): Promise<string> {
+    const response = await axios.post<TokenResponse>(
+      this.tokenUrl,
+      'grant_type=client_credentials', // Body as a URL-encoded string
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          Authorization: `Basic ${Buffer.from(
+            `${this.clientId}:${this.clientSecret}`,
+          ).toString('base64')}`,
+        },
+      },
+    );
+
+    return response.data.access_token;
+  }
   async verifyPayment(reference: string): Promise<any> {
     const response = await axios.get(
       `${this.interswitchBaseUrl}/transactions/verify/${reference}`,
