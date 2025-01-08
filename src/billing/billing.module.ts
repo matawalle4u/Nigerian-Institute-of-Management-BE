ab93793e4ab82d6c8b1b24cecee91d70b96a25aa
Adam Mustaoha <matawalle4u@gmail.com>
2025-01-08 22:06:07 +0100

    Registered Rpositories to the module

--- src/billing/billing.module.ts
+++ src/billing/billing.module.ts
@@ -1,4 +1,14 @@
+// src/billing/billing.module.ts
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BillingService } from './billing.service';
+import { BillingController } from './billing.controller';
+import { Bill } from './entities/bill.entity';
+import { Login } from 'src/account/entities/login.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Bill, Login])],
+  controllers: [BillingController],
+  providers: [BillingService],
+})
 export class BillingModule {}
