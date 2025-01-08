c60fdecf369df8adf98eef92fc93a9decd8f58d8
Adam Mustaoha <matawalle4u@gmail.com>
2025-01-08 22:04:55 +0100

    Billing create dto

--- src/billing/dto/create-billing.dto.ts
+++ src/billing/dto/create-billing.dto.ts
@@ -1,14 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsString, IsNotEmpty } from 'class-validator';
 
 export class CreateBillDto {
+  @ApiProperty({
+    description: 'A valid user ID',
+    example: '1',
+  })
   @IsString()
   @IsNotEmpty()
-  userId: string;
+  userId: number;
 
+  @ApiProperty({
+    description: 'Bill description',
+    example: 'Payment for upgrade fees',
+  })
   @IsString()
   @IsNotEmpty()
   description: string;
 
+  @ApiProperty({
+    description: 'Amount payable',
+    example: '10000',
+  })
   @IsNumber()
   @IsNotEmpty()
   amount: number;
