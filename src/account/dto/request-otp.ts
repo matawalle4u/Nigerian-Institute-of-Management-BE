93723d600a62888266ccef41b83e74bccecd1082
Adam Mustaoha <matawalle4u@gmail.com>
2025-01-14 07:20:32 +0100

    Added sample payload to swagger doc

--- src/account/dto/request-otp.ts
+++ src/account/dto/request-otp.ts
@@ -1,13 +1,36 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class RequestOtpDto {
+  @ApiProperty({
+    description: 'email',
+    example: 'adam.mustapha@byteflow.com.ng',
+  })
   email: string;
 }
 
 export class VerifyOtpDto {
+  @ApiProperty({
+    description: 'email',
+    example: 'adam.mustapha@byteflow.com.ng',
+  })
   email: string;
+  @ApiProperty({
+    description: 'OTP',
+    example: '1234',
+  })
   otp: string;
 }
 
 export class ResetPasswordDto {
+  @ApiProperty({
+    description: 'email',
+    example: 'adam.mustapha@byteflow.com.ng',
+  })
   email: string;
+
+  @ApiProperty({
+    description: 'New password',
+    example: 'Password1234',
+  })
   newPassword: string;
 }
