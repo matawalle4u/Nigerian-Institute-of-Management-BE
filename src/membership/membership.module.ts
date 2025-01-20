0c6cae23e1b980ede6ca55d45989d128c5d8ebfd
Adam Mustaoha <matawalle4u@gmail.com>
2025-01-20 17:58:47 +0100

    Registered Entities in the membership module typeorm

--- src/membership/membership.module.ts
+++ src/membership/membership.module.ts
@@ -3,9 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { MembershipService } from './membership.service';
 import { MembershipController } from './membership.controller';
 import { Members } from './entities/membership.entity';
+import { Criteria } from './entities/criteria.entity';
+import { Upgrade } from './entities/upgrade.entity';
+import { Grade } from './entities/grade.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Members])],
+  imports: [TypeOrmModule.forFeature([Members, Criteria, Grade, Upgrade])],
   controllers: [MembershipController],
   providers: [MembershipService],
 })
