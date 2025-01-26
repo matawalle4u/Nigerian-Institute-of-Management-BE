c3eb5afc5a366274ce4d72641fe28add41005d9f
Adam Mustaoha <matawalle4u@gmail.com>
2025-01-26 12:18:47 +0100

    Removed unwanted code

--- src/membership/entities/criteria.entity.ts
+++
@@ -1,9 +0,0 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
-
-@Entity('criteria')
-export class Criteria {
-  @PrimaryGeneratedColumn()
-  id: number;
-  @Column({ type: 'json' })
-  requirements: any;
-}
