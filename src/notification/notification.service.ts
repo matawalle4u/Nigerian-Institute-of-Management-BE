e4eff2b94b777fb06be0929af0a850442e79db38
Adam Mustaoha <matawalle4u@gmail.com>
2025-03-04 14:09:44 +0100

    Refactored to proper promise chaining

--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -2,7 +2,6 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Notification } from './entities/notification.entity';
-import { CreateNotificationDto } from './dto/create-notification.dto';
 import { CreateGeneralNotificationDto } from './dto/create-general-notification.dto';
 import { Login } from 'src/account/entities/login.entity';
 @Injectable()
@@ -13,46 +12,6 @@ export class NotificationService {
     @InjectRepository(Login)
     private readonly userRepository: Repository<Login>,
   ) {}
-
-  // createNotification(
-  //   login_id: number,
-  //   title: string,
-  //   message: string,
-  //   type: string,
-  //   // createNotificationDto: CreateNotificationDto,
-  // ): Promise<Notification> {
-  //   // const { loginId, title, message, type } = createNotificationDto;
-  //   // const notification = this.notificationRepository.create({
-  //   //   user: { id: login_id },
-  //   //   title,
-  //   //   message,
-  //   //   isRead: false,
-  //   //   type,
-  //   // });
-
-  //   return this.notificationRepository
-  //     .create({
-  //       user: { id: login_id },
-  //       title,
-  //       message,
-  //       isRead: false,
-  //       type,
-  //     })
-  //     .then((notification) => {
-  //       return this.notificationRepository.save(notification);
-  //     })
-  //     .catch((err) => {
-  //       throw new Error(err);
-  //     });
-
-  //   // return this.notificationRepository
-  //   // .save(notification)
-  //   // .then((result)=>{
-  //   //   return result;
-  //   // }).catch((error)=>{
-  //   //   throw new Error(error);
-  //   // });
-  // }
 
   createNotification(
     login_id: number,
@@ -95,11 +54,18 @@ export class NotificationService {
     return this.notificationRepository.save(notifications);
   }
 
-  async getNotificationsByLogin(loginId: number): Promise<Notification[]> {
-    return this.notificationRepository.find({
-      where: { user: { id: loginId } },
-      order: { createdAt: 'DESC' },
-    });
+  getNotificationsByLogin(loginId: number): Promise<Notification[]> {
+    return this.notificationRepository
+      .find({
+        where: { user: { id: loginId } },
+        order: { createdAt: 'DESC' },
+      })
+      .then((response) => {
+        return response;
+      })
+      .catch((error) => {
+        throw new Error(error);
+      });
   }
 
   async markAsRead(notificationId: number): Promise<Notification> {
