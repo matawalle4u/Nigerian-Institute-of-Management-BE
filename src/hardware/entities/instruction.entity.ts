bd71d55e25d813a085e9aafdb95eb4aa397ced82
Adam Mustaoha <matawalle4u@gmail.com>
2025-03-11 14:58:32 +0100

    Fixed Swagger doc for hardware

--- src/hardware/entities/instruction.entity.ts
+++ src/hardware/entities/instruction.entity.ts
@@ -1,17 +1,24 @@
 import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
 import { Pin } from './pin.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity()
 export class Instruction {
   @PrimaryGeneratedColumn()
+  @ApiProperty({ description: 'Unique identifier for the instruction' })
   id: number;
 
   @Column()
+  @ApiProperty({ description: 'Node ID associated with the instruction' })
   nodeId: number;
 
   @OneToMany(() => Pin, (pin) => pin.instruction, {
     cascade: true,
     eager: true,
+  })
+  @ApiProperty({
+    type: () => [Pin],
+    description: 'List of pins associated with this instruction',
   })
   pins: Pin[];
 }
--- src/hardware/entities/pin.entity.ts
+++ src/hardware/entities/pin.entity.ts
@@ -1,17 +1,22 @@
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
 import { Instruction } from './instruction.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity()
 export class Pin {
   @PrimaryGeneratedColumn()
+  @ApiProperty({ description: 'Unique identifier for the pin' })
   id: number;
 
   @Column()
+  @ApiProperty({ description: 'Pin number associated with the instruction' })
   pin: number;
 
   @Column()
+  @ApiProperty({ description: 'State of the pin (e.g., 1 for ON, 0 for OFF)' })
   state: number;
 
   @ManyToOne(() => Instruction, (instruction) => instruction.pins)
+  @ApiProperty({ description: 'Reference to the associated instruction' })
   instruction: Instruction;
 }
