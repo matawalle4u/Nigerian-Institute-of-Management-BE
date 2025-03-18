5ca6e9b72b2d00125b6f6354aeea4f2e8fb3f197
Adam Mustaoha <matawalle4u@gmail.com>
2025-03-18 12:32:46 +0100

    Added FirebaseService to list of providers

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -4,10 +4,11 @@ import { NotificationService } from './notification.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Notification } from './entities/notification.entity';
 import { Login } from 'src/account/entities/login.entity';
+import { FirebaseService } from './firebase.service';
 @Module({
   imports: [TypeOrmModule.forFeature([Notification, Login])],
   controllers: [NotificationController],
-  providers: [NotificationService],
+  providers: [NotificationService, FirebaseService],
   exports: [NotificationService],
 })
 export class NotificationModule {}
